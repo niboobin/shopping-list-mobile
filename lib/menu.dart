65cdcbb9722fc0ae76f26a22d6e25e25f8499a8b
niboobin <obinnmandalikaa@gmail.com>
2023-11-02 11:41:31 +0700

    Tutorial 6

--- lib/menu.dart
+++ lib/menu.dart
@@ -1,87 +1,113 @@
 import 'package:flutter/material.dart';
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key? key}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  final List<ShopItem> items = [
+    ShopItem("View Products", Icons.checklist),
+    ShopItem("Add Product", Icons.add_shopping_cart),
+    ShopItem("Logout", Icons.logout),
+    ];
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+  appBar: AppBar(
+    title: const Text(
+      'Shopping List',
+    ),
+  ),
+  body: SingleChildScrollView(
+    // Scrolling wrapper widget
+    child: Padding(
+      padding: const EdgeInsets.all(10.0), // Set padding for the page
+      child: Column(
+        // Widget to display children vertically
+        children: <Widget>[
+          const Padding(
+            padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+            // Text widget to display text with center alignment and appropriate style
+            child: Text(
+              'PBP Shop', // Text indicating the shop name
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          // Grid layout
+          GridView.count(
+            // Container for our cards.
+            primary: true,
+            padding: const EdgeInsets.all(20),
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+            crossAxisCount: 3,
+            shrinkWrap: true,
+            children: items.map((ShopItem item) {
+              // Iteration for each item
+              return ShopCard(item);
+            }).toList(),
+          ),
+        ],
+      ),
+    ),
+  ),
+);
+    }
+
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class ShopItem {
+  final String name;
+  final IconData icon;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  ShopItem(this.name, this.icon);
+}
+
+class ShopCard extends StatelessWidget {
+  final ShopItem item;
+
+  const ShopCard(this.item, {Key? key}); // Constructor
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return Material(
+      color: Colors.indigo,
+      child: InkWell(
+        // Responsive touch area
+        onTap: () {
+          // Show a SnackBar when clicked
+          ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(SnackBar(
+                content: Text("You pressed the ${item.name} button!")));
+        },
+        child: Container(
+          // Container to hold Icon and Text
+          padding: const EdgeInsets.all(8),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  item.icon,
+                  color: Colors.white,
+                  size: 30.0,
+                ),
+                const Padding(padding: EdgeInsets.all(3)),
+                Text(
+                  item.name,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
